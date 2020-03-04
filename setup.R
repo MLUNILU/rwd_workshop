6ddabe3408e788b9c43771b07992f008ba7618f0
MLUNILU <maximilian.lueth@unilu.ch>
2020-03-04 16:24:33 +0100

    Initial commit workshop

---
+++ setup.R
@@ -0,0 +1,9 @@
+install.packages(c("rmarkdown","tinytex","dplyr","ggplot2","stargazer"))
+
+library(rmarkdown)
+library(tinytex)
+library(dplyr)
+library(ggplot2)
+library(stargazer)
+
+tinytex::install_tinytex()
